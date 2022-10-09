cd562e98f7a8811dc03395835f77094067ebb76d
Richard Mayers <rmayers68@gmail.com>
2022-10-09 01:09:01 -0700

    JSON Category, Drink List, and Drinks all able to be deserialized properly.

--- Drink Menu/Program.cs
+++ Drink Menu/Program.cs
@@ -1,74 +1,96 @@
 ﻿using ConsoleTableExt;
-using Drink_Menu;
-using System.Net.Http.Headers;
 using System.Text.Json;
 
-namespace Flashcards
+namespace Drink_Menu
 {
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
-            await ProcessRepositories();
+            string stackSelection;
             bool closeProgram = false;
             while (!closeProgram)                                                     //MAIN MENU
             {
-                string stackSelection = "";
-                string[] categories = { "Categories Menu", "Ordinary Drink", "Cocktail", "Milk/Float/Shake", "Other/Unknown", "Cocoa", "Shot", "Coffee/Tea", "Homemade Liqeur", "Punch,Party Drink", "Beer", "Soft Drink/Soda" };
-                List<string> list = new(categories);
-                ConsoleTableBuilder.From(list).ExportAndWriteLine();
-                stackSelection = DataValidation.EmptyStringCheck();
-                switch (stackSelection)
+                Console.Clear();
+                List<string> list = await ProcessJSONRequest("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list");
+                Console.WriteLine("Please enter a Category you would like to choose (type exactly as shown) or input Exit to terminate:");
+                stackSelection = TextCheck(list);
+                bool goBack = false;
+                string drinkChoice;
+                if (stackSelection == "Exit")
                 {
-                    default:
-                        Console.WriteLine("Goodbye!");
+                    Console.WriteLine("Goodbye!");
+                    Console.ReadKey();
+                    closeProgram = true;
+                }
+                 else while (!goBack)
+                {
+                    list.Clear();
+                    Console.Clear();
+                    list = await ProcessJSONRequest($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={stackSelection}");
+                    Console.WriteLine("Please enter the drink you would like to know more about or enter \"Go Back\":");
+                    drinkChoice = TextCheck(list);
+                    if (drinkChoice == "Go Back")
+                        goBack = true;
+                    else
+                    {
+                        Console.Clear();
+                        await ProcessJSONRequest($"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drinkChoice}");
+                        Console.WriteLine("Press any key to return to drink list...");
                         Console.ReadKey();
-                        closeProgram = true;
-                        break;
+                    }                   
                 }
             }
-                   
         }
 
-        // Menu UI Methods
-        public static string Tab(int i)
+
+
+        // Data Validation
+        public static string TextCheck(List<string> list)
         {
-            string tab = "";
-            for (int j = 0; j < i; j++)
-                tab += "\t";
-            return tab;
+            while (true)
+            {
+                string a = Console.ReadLine();
+                if (a == "Go Back" || a == "Exit")
+                    return a;
+                foreach (var category in list)
+                    if (category == a)
+                        return a;
+                Console.WriteLine("Invalid input, please enter the correct name: ");
+            }
         }
 
-        public static string MenuLine()
+        //JSON Object Handling
+        private static async Task<List<string>> ProcessJSONRequest(string URL)
         {
-            string a = $"{Tab(5)}********************************";
-            return a;
+            var streamTask = client.GetStreamAsync(URL);
+            var choices = await JsonSerializer.DeserializeAsync<JSON>(await streamTask);
+            List<string> list = new();
+            if (URL.Contains("c=list"))                                 // Category
+            {
+                foreach (var x in choices.drinks)
+                    list.Add(x.strCategory);
+                ConsoleTableBuilder.From(list).WithTitle("Category Menu").ExportAndWriteLine(TableAligntment.Center);
+            }
+            else if (URL.Contains("s="))                                // Drink
+            {
+                foreach(var x in choices.drinks)
+                {
+                    list.Add($"Name: {x.strDrink}");
+                    list.Add($"{x.strAlcoholic}");
+                    list.Add($"Glass: {x.strGlass}");
+                    list.Add($"Ingredient 1: {x.strMeasure1} {x.strIngredient1}");
+                }
+                ConsoleTableBuilder.From(list).ExportAndWriteLine();
+            }
+            else                                                        // Drink List
+            {
+                foreach (var x in choices.drinks)
+                    list.Add(x.strDrink);
+                ConsoleTableBuilder.From(list).WithTitle("Category Menu").ExportAndWriteLine(TableAligntment.Center);
+            }
+            return list;
         }
-        public static void Pause()
-        {
-            Console.WriteLine("\nPress any key to continue...\n");
-            Console.ReadKey();
-        }
-
-        private static async Task ProcessRepositories()
-        {
-
-            /*client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-            */
-
-            var streamTask = client.GetStreamAsync("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list");
-            var categories = await JsonSerializer.DeserializeAsync<Categories>(await streamTask);
-
-
-            /*var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);System.Text.Json.JsonException: 'The JSON value could not be converted to System.Collections.Generic.List`1[Drink_Menu.Repository]. Path: $ | LineNumber: 0 | BytePositionInLine: 1.'
-            */
-
-            Console.WriteLine(categories.strCategory);
-        }
-    }
+    }    
 }
--- Drink Menu/repo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Drink_Menu
-{
-    public class Categories
-    {
-        public string strCategory { get; set; }
-    }
-}
